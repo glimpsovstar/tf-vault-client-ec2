8e8c49146ac4d339c65fa164c92a627070d38370
David Joo <david.joo@hashicorp.com>
2024-10-30 00:27:12 +1100

    first commit

---
+++ README.md
@@ -0,0 +1,18 @@
+# tf-vault-client-ec2
+
+---
+
+This is a simple terraform code respository to create RHEL9 AWS ec2 instance will serve as a HCP Vault Client.
+
+This will help demonstrate benefits of:
+
+- Infrastructure as Code
+- Terraform
+- Terraform Cloud (TFC)
+- Vault agent
+
+## Prerequisite
+
+---
+
+This code is expecting, "vpc-id" from TFC's "djoo-hashicorp"organisation "hcp-vault-demo-hvn-tgw" workspace.
---
+++ main.tf
@@ -0,0 +1,24 @@
+resource "aws_instance" "rhel9_instance" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  subnet_id     = data.terraform_remote_state.aws_dev_vpc.outputs.vpc_public_subnets
+  key_name      = var.aws_key_pair_name
+  tags          = var.ec2_tags
+  vpc_security_group_ids = [data.terraform_remote_state.aws_dev_vpc.outputs.security_group-ssh_http_https_allowed] 
+}
+
+## TESTING with aws_ec2_instance_state ####
+# As it is stated on: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ec2_instance_state. ##
+# "AWS does not currently have an EC2 API operation to determine an instance has finished processing user data. As a result, this resource can interfere with user data processing." ##
+# For the demo purposes, I may choose to have the ec2 instance stopped, and for the demo purpose, I may want it to be up and runing.##
+###########################################
+
+resource "aws_ec2_instance_state" "rhel9_instance_state" {
+  instance_id = aws_instance.rhel_instance.id
+  state = "running"
+}
+
+resource "aws_eip" "instance-eip" {
+  instance = aws_instance.rhel9_instance.id
+  vpc = true
+}
